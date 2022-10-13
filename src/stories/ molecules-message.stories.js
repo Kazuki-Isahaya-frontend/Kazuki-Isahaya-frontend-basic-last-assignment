73e325a03f04b6128252e5a91ae85ec269a5c677
Kazuki-Isahaya-frontend <k.isahaya@4nonome.com>
2022-10-13 14:36:10 +0900

    molecules-message

--- src/stories/ molecules-message.stories.js
+++ src/stories/ molecules-message.stories.js
@@ -1,6 +1,6 @@
-import Item_Example from "../components/molecules/message/message.html";
+import Item_Message from "../components/molecules/message/message.html";
 import "../components/molecules/message/message.scss";
 
-export default { title: "Atoms/Example" }
+export default { title: "Molecules/Message" }
 
-export const Example = () => Item_Example;
+export const Example = () => Item_Message;
