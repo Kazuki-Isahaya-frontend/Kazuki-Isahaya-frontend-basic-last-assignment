7f0fceeb1924ab81719017777e5f4b0d82228bf7
Kazuki-Isahaya-frontend <k.isahaya@4nonome.com>
2022-10-24 11:36:19 +0900

    message kansei?

--- src/stories/atoms-button.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Button from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "Atoms/Example" }
-
-export const Example = () => Item_Button;
